505aebae608716c6c46f59add17271bbc1cdb7d9
Marat Nuriev <nuriev@nsolutions.ru>
2017-08-21 08:55:19 +0300

    styles upgrade

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,21 +1,21 @@
 let build = require('../index.js');
 // let scss = require('../plugins/scss');
 // let copy = require('../plugins/copy');
-// let styles = require('../plugins/styles');
+let styles = require('../plugins/styles');
 //let js = require('../plugins/js');
 //let webpack = require('../plugins/webpack');
 //let compress = require('../plugins/compress');
-let sprite = require('../plugins/sprite');
+//let sprite = require('../plugins/sprite');
 
 build(function(query) {
   query
     .plugins([
       // scss
       // , copy
-      // , styles
+      , styles
       //, webpack
       //, compress
-      , sprite
+      //, sprite
       //, js
     ])
     // SCSS
@@ -32,27 +32,32 @@ build(function(query) {
     //.scss("scss/app.scss", "css/", 'app')
     //.copy(['scss/app.scss','scss/test_scss.scss'],"cs/t.scss")
     //.copy('scss/**/*',"css/o/")
-    //.scss("scss/*.scss","css/",'tt')
-    //.styles(['1.css','2.css'],'css/12.css',{parent_folder:'css_source/'})
     //.copy(['css_source/1.css', 'css_source/2.css'], 'css/copy_12.css')
+    //.scss("scss/*.scss","css/",'tt')
+
+    //.styles(['1.css','2.css'],'css/12.css',{parent_folder:'css_source/'})
+    //.styles('css_source/*.css','css/')
+    .styles('css_source/**/*.css','css/big.css')
 
     //.js(['js_source/admin.js', 'js_source/app.js'], 'js/full.js')
     //.js('js_source/app.js', 'js/app.js')
 
     //.webpack('js_source/w.js', 'js/w.compile.js')
+
     // .compress([
     //   {from:'images_source/auth/*.png',to: 'auth/'},
     //   {from:'images_source/watch/*',to: 'watch/'},
     // ], 'images/')
-    .sprite([
-      "images_source/watch/*.jpg",
-      "images_source/auth/*.png"
-    ], {
-      images: "images/",
-      css: "json/"
-    }, {
-      extension: "json",
-      format: "json_texture"
-    })
+
+    // .sprite([
+    //   "images_source/watch/*.jpg",
+    //   "images_source/auth/*.png"
+    // ], {
+    //   images: "images/",
+    //   css: "json/"
+    // }, {
+    //   extension: "json",
+    //   format: "json_texture"
+    // })
   ;
 });
